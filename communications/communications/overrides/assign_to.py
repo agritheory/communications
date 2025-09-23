9c251e681c80c2a8044ec523c8f75a256b2b9543
fproldan <franciscoproldan@gmail.com>
2025-09-23 15:33:45 +0000

    feat: Allow configured Notification to be used instead of only email for notify_assignment

---
+++ communications/communications/overrides/assign_to.py
@@ -0,0 +1,64 @@
+import frappe
+from frappe import _
+from frappe.desk.doctype.notification_log.notification_log import (
+	enqueue_create_notification,
+	get_title,
+	get_title_html,
+)
+
+
+def custom_notify_assignment(
+	assigned_by, allocated_to, doc_type, doc_name, action="CLOSE", description=None
+):
+	"""
+	Monkey patch for notify_assignment to use Notification DocType if configured.
+	"""
+	if not (assigned_by and allocated_to and doc_type and doc_name):
+		return
+
+	assigned_user = frappe.db.get_value("User", allocated_to, ["language", "enabled"], as_dict=True)
+
+	# return if self assigned or user disabled
+	if assigned_by == allocated_to or not assigned_user.enabled:
+		return
+
+	# Check if a Notification is configured for ToDo
+	notification_name = frappe.db.get_value("Notification", {"document_type": "ToDo", "enabled": 1})
+
+	if notification_name:
+		try:
+			notification = frappe.get_doc("Notification", notification_name)
+			notification.send(doc=frappe.get_doc(doc_type, doc_name))
+			return
+		except Exception as e:
+			frappe.log_error(
+				f"Failed to send Notification DocType for assignment: {str(e)}", "custom_notify_assignment"
+			)
+
+	# Search for email address in description -- i.e. assignee
+	user_name = frappe.get_cached_value("User", frappe.session.user, "full_name")
+	title = get_title(doc_type, doc_name)
+	description_html = f"<div>{description}</div>" if description else None
+
+	if action == "CLOSE":
+		subject = _(
+			"Your assignment on {0} {1} has been removed by {2}", lang=assigned_user.language
+		).format(frappe.bold(_(doc_type)), get_title_html(title), frappe.bold(user_name))
+	else:
+		user_name = frappe.bold(user_name)
+		document_type = frappe.bold(_(doc_type, lang=assigned_user.language))
+		title = get_title_html(title)
+		subject = _("{0} assigned a new task {1} {2} to you", lang=assigned_user.language).format(
+			user_name, document_type, title
+		)
+
+	notification_doc = {
+		"type": "Assignment",
+		"document_type": doc_type,
+		"subject": subject,
+		"document_name": doc_name,
+		"from_user": frappe.session.user,
+		"email_content": description_html,
+	}
+
+	enqueue_create_notification(allocated_to, notification_doc)
